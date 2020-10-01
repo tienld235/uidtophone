a87fe876d877494cdcb9b2b02031ac6998c203e5
tienld <tienld@vietnamlab.vn>
2020-10-01 16:02:57 +0700

    handle api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Vnphone;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/vnphone/{id}', function (Request $request) {
+    $phoneObj = Vnphone::where('uid',$request->id)->first();
+    $message = $phoneObj !==null ? "Phone for $request->id" : "No matching!";
+    $phone = $phoneObj !==null ? $phoneObj->phone : null;
+    return [
+        "message" => $message,
+        "data" => $phone
+    ];
 });
+
+Route::post("excel", function(Request $request) {
+    if (request()->hasFile('excel_file')) {
+        $file = request()->file('excel_file');
+        $path = public_path().'/uploads/';
+
+        $file->move($path, $file->getClientOriginalName());
+        $spreadsheet = IOFactory::load($path.$file->getClientOriginalName());
+
+        $worksheet = $spreadsheet->getActiveSheet();
+        $highestRow = $worksheet->getHighestRow();
+
+        for ($row = 2; $row <= $highestRow; ++$row) {
+            $phoneObj =  Vnphone::where('uid',$worksheet->getCell('A'.$row)->getValue())->first();
+            $phone = $phoneObj !== null ? $phoneObj->phone : '';
+            $worksheet->getCell('B'.$row)->setValue($phone);
+        }
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($path.$file->getClientOriginalName());
+
+        return response()->download($path.$file->getClientOriginalName())->deleteFileAfterSend(true);;
+    }
+});
+
